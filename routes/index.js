fe8620fb16d00b646ee801a5c45b144f91e45d65
Rangga Eki Syahrial <58381367+resyahrial@users.noreply.github.com>
2021-09-24 15:48:15 +0700

    Feature upload file (#6)
    * Create endpoint signed url
    
    * Fix object url
    
    * fix http method

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,17 @@
 const router = require('express').Router()
+
 const CityController = require('../controllers/cityController');
-const driver = require('./driverRouter')
+const UploadController = require("../controllers/uploadController");
+
+const driversRouter = require('./driverRouter')
 const trucksRouter = require("./trucks");
-router.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-router.use('/drivers', driver)
+
+router.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+router.use('/drivers', driversRouter)
 router.use("/trucks", trucksRouter);
+router.post("/signed-url", UploadController.getSignedUrl);
 router.get('/cities', CityController.getCity)
 
 module.exports = router;
