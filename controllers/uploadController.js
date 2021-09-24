b0d8e0b7210ccdb9fb77a6d8eb420fb3979f95f0
resyahrial <syahrialrangga11@gmail.com>
2021-09-24 14:02:07 +0700

    Create endpoint signed url

---
+++ controllers/uploadController.js
@@ -0,0 +1,33 @@
+const AWS = require("aws-sdk");
+const { v4: uuid } = require("uuid");
+
+const s3 = new AWS.S3({
+  region: process.env.AWS_REGION,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+});
+
+class Controller {
+  static async getSignedUrl(req, res, next) {
+    try {
+      const { type, extension } = req.body;
+      let key = type + "/" + uuid() + "." + extension;
+      const uploadUrl = await s3.getSignedUrl("putObject", {
+        Bucket: process.env.S3_BUCKETNAME,
+        Key: key,
+        Expires: process.env.SIGNED_URL_EXPIRE,
+        ACL: "public-read",
+        ContentType: "application/json",
+      });
+
+      res.status(201).json({
+        uploadUrl,
+        objectUrl: process.env.S3_ENDPOINT + key,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+module.exports = Controller;
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
 const router = require('express').Router()
+
 const CityController = require('../controllers/cityController');
-const driver = require('./driverRouter')
+const UploadController = require("../controllers/uploadController");
+
+const driversRouter = require('./driverRouter')
 const trucksRouter = require("./trucks");
-router.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-router.use('/drivers', driver)
+
+router.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+router.get("/signed-url", UploadController.getSignedUrl);
+router.use('/drivers', driversRouter)
 router.use("/trucks", trucksRouter);
 router.get('/cities', CityController.getCity)
 
