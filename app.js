246471f9d5773a34f601e135b7872c4194610347
burhan yusuf <burhanyusuf@outlook.com>
2021-09-24 08:36:43 +0700

    Initialization express project and folder structure

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const route = require('./routes')
+const cors = require('cors');
+
+app.use(cors())
+app.use(route)
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
